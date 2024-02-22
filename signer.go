7e5fcb57294ae3a5ed43e453b10a758b7fc46aab
Administrator <admin@example.com>
2024-02-22 22:01:27 +0300

    main logic

--- signer.go
+++ signer.go
@@ -1,17 +1,68 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
+
+type job func(in, out chan interface{})
+
 func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{})
+	out := make(chan interface{}, 2)
 
+	wg := &sync.WaitGroup{}
+
+	for _, jobFunc := range jobs {
+		wg.Add(1)
+		go func(jobFunc job, in, out chan interface{}) {
+			defer wg.Done()
+			jobFunc(in, out)
+		}(jobFunc, in, out)
+		in = out
+		out = make(chan interface{}, 2)
+	}
+	wg.Wait()
+	close(out)
 }
 
-func SingleHash() {
-
+func SingleHash(in, out chan interface{}) {
+	go func() {
+		for n := range in {
+			out <- n.(int) * n.(int)
+		}
+	}()
 }
 
-func MultiHash() {
-
+func MultiHash(in, out chan interface{}) {
+	go func() {
+		for n := range in {
+			fmt.Println(n)
+			out <- n.(int) + 1
+		}
+	}()
 }
 
-func CombineResults() {
+func CombineResults(in, out chan interface{}) {
+	go func() {
+		for n := range in {
+			fmt.Println(n)
+			out <- n.(int) + 1
+		}
+	}()
+}
 
+func main() {
+	inputData := []int{0, 1, 1, 2, 3, 5, 8}
+	hashSignJobs := []job{
+		job(func(in, out chan interface{}) {
+			for _, fibNum := range inputData {
+				out <- fibNum
+			}
+		}),
+		job(SingleHash),
+		job(MultiHash),
+		job(CombineResults),
+	}
+	ExecutePipeline(hashSignJobs...)
 }
